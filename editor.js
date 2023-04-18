265e11fe5c6832c6060afd0631a815ffd8e51102
Ayodeji Akintubi <93990522+OldieCoder@users.noreply.github.com>
2023-04-18 13:45:24 +0100

    Add files via upload

--- editor.js
+++ editor.js
@@ -1,24 +1,106 @@
-import React, { useRef } from "react";
-import JoditEditor from "jodit-react";
+import React, { useState } from 'react';
+import './index.css';
 
-// const config = {
-//   buttons: ["bold", "italic",  "underline",]
-// };
+function TextEditor() {
+  const [content, setContent] = useState('');
+  const [isBold, setIsBold] = useState(false);
+  const [isItalic, setIsItalic] = useState(false);
+  const [isCentered, setIsCentered] = useState(false);
+  const [isRightAligned, setIsRightAligned] = useState(false);
+  const [isLeftAligned, setIsLeftAligned] = useState(false);
+  const [isLowerCase, setIsLowerCase] = useState(false);
+  const [isUpperCase, setIsUpperCase] = useState(false);
 
+  const handleInputChange = (e) => {
+    setContent(e.target.value);
+  };
 
+  const handleBoldClick = () => {
+    setIsBold(!isBold);
+  };
 
-const Editor = ({ initialValue, getValue }) => {
-  const editor = useRef(null);
+  const handleItalicClick = () => {
+    setIsItalic(!isItalic);
+  };
+
+  const handleLowercaseClick = () => {
+    setIsLowerCase(!isLowerCase);
+  };
+
+  const handleUppercaseClick = () => {
+    setIsUpperCase(!isUpperCase);
+  };
+
+  const handleAlignCenterClick = () => {
+    setIsCentered(!isCentered);
+  };
+
+  const handleAlignRightClick = () => {
+    setIsRightAligned(!isRightAligned);
+  };
+
+  const handleAlignLeftClick = () => {
+    setIsLeftAligned(!isLeftAligned);
+  };
+
+  const getStyle = () => {
+    let style = {};
+    if (isBold) {
+      style.fontWeight = 'bold';
+    }
+    if (isItalic) {
+      style.fontStyle = 'italic';
+    }
+    if (isCentered) {
+      style.textAlign = 'center';
+    }
+    if (isRightAligned) {
+      style.textAlign = 'right';
+    }
+    if (isLeftAligned) {
+      style.textAlign = 'left';
+    }
+    if (isLowerCase) {
+      return { ...style, textTransform: 'lowercase' };
+    }
+    if (isUpperCase) {
+      return { ...style, textTransform: 'uppercase' };
+    }
+    return style;
+  };
 
   return (
-    <JoditEditor
-      ref={editor}
-      value={initialValue}
-      config={config}
-       tabIndex={1}
-           onChange={(newContent) => getValue(newContent)}
-    />
+    <div className="App">
+      <input
+        type="text"
+        className = "text-editor"
+        value={content}
+        onChange={handleInputChange}
+        style={getStyle()}
+      />
+      <div className = "grid-container">
+        <button className = "btn" onClick={handleBoldClick}>{isBold ? 'Unbold' : 'Bold'}</button>
+        <button className = "btn" onClick={handleItalicClick}>
+          {isItalic ? 'Unitalicize' : 'Italicize'}
+        </button>
+        <button className = "btn" onClick={handleLowercaseClick}>
+          {isLowerCase ? 'Normal case' : 'Lowercase'}
+        </button>
+        <button className = "btn" onClick={handleUppercaseClick}>
+          {isUpperCase ? 'Normal case' : 'Uppercase'}
+        </button>
+        <button className = "btn" onClick={handleAlignCenterClick}>
+          {isCentered ? 'Uncenter' : 'Center'}
+        </button>
+        <button className = "btn" onClick={handleAlignRightClick}>
+          {isRightAligned ? 'Unright align' : 'Right align'}
+        </button>
+        <button className = "btn" onClick={handleAlignLeftClick}>
+          {isLeftAligned ? 'Unleft align' : 'Left align'}
+        </button>
+      </div>
+    </div>
   );
-};
+}
 
-export default Editor;
+export default TextEditor;
